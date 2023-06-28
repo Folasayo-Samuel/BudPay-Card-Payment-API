d7e31cb9bd6f14ebf9c97d3595aea3f45983dbc2
FOLASAYO SAMUEL OLAYEMI <61091636+Folasayo-Samuel@users.noreply.github.com>
2023-06-28 23:51:25 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 module.exports = {
   apiPublicKey: process.env.BUDPAY_PUBLIC_KEY,
-  apiSecret: process.env.BUDPAY_SECRET_KEY,
+  apiSecretKey: process.env.BUDPAY_SECRET_KEY,
 };
